6e3c7ef4267df99dc62fbaf23bd5006da1148312
AREL <AREL@B107-05>
2018-02-23 14:48:04 +0300

    DataGridAdd

--- WpfDataGridExap/MainWindow.xaml.cs
+++ WpfDataGridExap/MainWindow.xaml.cs
@@ -13,16 +13,55 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfDataGridExap
+namespace WpfSample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Customers> list;
         public MainWindow()
         {
             InitializeComponent();
+            list = new List<Customers>()
+            {
+                new Customers() { AD="Ali",SOYAD="Kazık" },
+                new Customers() { AD="Veli",SOYAD="Veled" },
+                new Customers() { AD="Vedat",SOYAD="Edat" },
+                new Customers() { AD="Mami",SOYAD="Taze" },
+                new Customers() { AD="Mavi",SOYAD="Pembe" },
+            };
+            Customers liste = new Customers();
+            {
+                liste.AD = "Caner";
+                liste.SOYAD = "Patates";
+          
+                list.Add(liste);
+            }
+
+        }
+
+        private void btntikla_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Butona Tıklandı.");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(cmbBoxListe.SelectedItem.ToString());
+
+            MessageBox.Show(((ComboBoxItem)(cmbBoxListe.SelectedItem)).Content.ToString());
+        }
+        public void Window_Load(object sender, RoutedEventArgs e)
+        {
+            tb1.ItemsSource = list;
         }
     }
+internal class Customers
+{
+    public string AD { get; internal set; }
+    public string SOYAD { get; internal set; }
+}
+    
 }
